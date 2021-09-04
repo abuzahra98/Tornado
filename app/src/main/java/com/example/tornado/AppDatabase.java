1976774a61c994e2bcff1b175ba89df6f7f696b5
abuzahra98 <dawoodbasher@gmail.com>
2021-09-04 18:03:33 +0300

    edit

--- app/src/main/java/com/example/tornado/AppDatabase.java
+++ app/src/main/java/com/example/tornado/AppDatabase.java
@@ -3,7 +3,7 @@ package com.example.tornado;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
-
+// AppDatabase class to hold the database
 @Database(entities = {Dish.class},version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract DishDao dishDao();
